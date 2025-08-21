fcc9b5bf836df886f5e4d06110461000d3c2431a
Frank Bergmann <frank.bergmann@project-open.com>
2025-08-21 09:48:58 +0000

    Replaced certificate with a placeholder Cert is now created in build.bash

--- build.bash
+++ build.bash
@@ -33,6 +33,12 @@ echo "===== git pull"
 git pull
 echo "===== git submodule update --recursive --init"
 git submodule update --recursive --init
+
+
 echo "===== cd $CURPWD"
 cd $CURPWD
+
+
+echo "===== Creating self-signed certificate"
+openssl req -x509 -newkey rsa:4096 -keyout openacs/etc/privkey.pem -out openacs/etc/certificate.pem -sha256 -days 3650 -nodes -subj "/C=ES/ST=Catalonia/L=Barcelona/O=Project Open Business Solutions, S.L./CN=project-open-v52.project-open.net"
 
